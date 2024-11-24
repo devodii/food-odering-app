c5bf5a906e9f66f94d1c7b33e2bd99ebe3dc0358
devodii <emmanuelodii80@gmail.com>
2024-11-24 08:06:55 +0100

    feat: product details screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,15 +1,93 @@
 import { Stack, useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import products from "@/assets/data/products";
+import { defaultPizzaImage } from "@/src/components/ProductListItem";
+import { useState } from "react";
+import { Button } from "@components/Button";
+
+const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailScreen = () => {
   const { id } = useLocalSearchParams();
+  const [size, setSize] = useState("M");
+
+  const addToCart = () => {
+    console.warn("Adding to cart..");
+  };
+
+  const product = products.find((it) => it.id.toString() == id);
+
+  if (!product) return <Text>Not found</Text>;
 
   return (
-    <View>
-      <Stack.Screen options={{ title: "Details" }} />
-      <Text>Product detail screen - {id}</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        style={styles.image}
+        source={{ uri: product.image ?? defaultPizzaImage }}
+      />
+
+      <Text>Select size</Text>
+
+      <View style={styles.sizes}>
+        {sizes.map((el) => (
+          <Pressable
+            onPress={() => setSize(el)}
+            key={el}
+            style={[
+              styles.size,
+              size === el && { backgroundColor: "gainsboro" },
+            ]}
+          >
+            <Text
+              style={[styles.stylesText, size === el && { color: "black" }]}
+            >
+              {el}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+
+      <Text style={styles.price}>${product.price}</Text>
+      <Button text="Add to cart" onPress={addToCart} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: "auto",
+  },
+  sizes: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: "white",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "gray",
+  },
+  stylesText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+});
 
 export default ProductDetailScreen;
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -3,7 +3,7 @@ import Colors from "@/src/constants/Colors";
 import { Product } from "../types";
 import { Link } from "expo-router";
 
-const defaultPizzaImage =
+export const defaultPizzaImage =
   "https://notjustdev-dummy.s3.us.east-2.amazonaws.com/food/default.png";
 
 export const ProductListItem = ({ product }: { product: Product }) => {
